887c8d3bf89690ec59a8a43154bedc815ee2ec79
smh <smharbort@outlook.com>
2024-10-04 11:00:06 -0400

    working thru part 3-- was working in wrong repo, I had a duplicate after trying to troubleshoot some git issues

--- data-collections/index.js
+++ data-collections/index.js
@@ -39,6 +39,47 @@ for (let i = 0; i < fullTable.length; i++) {
     console.log(fullTable[i])                           // simple loop thru array to print each cell to console after printing all column headers
 }
 
-//                                                              PART TWO: Expanding
+//                                                              PART TWO: Expanding Functionality
+function parseCSVData (string) {
 
-// test test
+    const CSVLines = string.split('\n')
+    const parsedByCells = []
+
+    for (let i = 0; i < CSVLines.length; i++) {
+
+        parsedByCells.push(CSVLines[i].split(','))
+
+    }
+
+    return parsedByCells
+}
+
+const parsedCSVString = parseCSVData(CSVString)
+// console.log(parsedCSVString)
+
+/* const CSVString2 = `Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232`
+const parsedCSVString2 = parseCSVData(CSVString2)
+console.log(parsedCSVString2)
+console.log(typeof parsedCSVString2) */
+
+//                                                              PART THREE: Transforming Data
+
+function arrToObj (array3D) {
+
+    newParsedByCells = []
+
+    for (let i = 0; i < array3D[0].length; i++) {
+
+        console.log(array3D[0][i])
+    }
+
+    array3D.forEach((object_ify) => {
+        // console.log('does work or?')
+
+        
+        newParsedByCells.push({})
+    })
+
+}
+
+const tryThis = arrToObj(parsedCSVString)
